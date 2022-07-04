766557e9e053165d6eb8921901e428a4d19ccab0
Griotold <gotjd9773@naver.com>
2022-07-04 09:56:13 +0900

    7월 4일 월요일

---
+++ VampPa_Board(Oracle)/src/test/java/com/vam/persistence/JDBCTest.java
@@ -0,0 +1,40 @@
+package com.vam.persistence;
+
+import static org.junit.Assert.fail;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import org.junit.Test;
+
+public class JDBCTest {
+
+    static { 
+        try { 
+            Class.forName("oracle.jdbc.driver.OracleDriver"); 
+        } catch(Exception e) { 
+            e.printStackTrace(); 
+        } 
+    } 
+    
+    @Test 
+    public void testConnection() {
+       
+       // given, when, then
+       
+       // given
+       String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+       String id = "griotold";
+       String password = "system123";
+       
+       // when
+        try(Connection con = DriverManager.getConnection( 
+              url, 
+                id, 
+                password)){
+           
+           // then
+            System.out.println(con); 
+        } catch (Exception e) { 
+            fail(e.getMessage()); 
+        } 
+    }    
+}
